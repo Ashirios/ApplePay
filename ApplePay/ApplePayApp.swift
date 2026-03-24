f8f4603625accdd8af87fe622d8084bf38bf2c80
Ashirios <arishasham473@gmail.com>
2026-03-24 10:10:12 +0600

    Initial Commit

---
+++ ApplePay/ApplePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePayApp.swift
+//  ApplePay
+//
+//  Created by alina on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ApplePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplePayTests/ApplePayTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePayTests.swift
+//  ApplePayTests
+//
+//  Created by alina on 24.03.2026.
+//
+
+import Testing
+@testable import ApplePay
+
+struct ApplePayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
